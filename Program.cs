898729cb70269dc1de42320f938aac0ccc80b127
Kacper Szuster <kacperszuster16@gmail.com>
2025-04-07 01:41:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShipGame
+{
+    public class Grid
+    {
+        public void showGrid()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
