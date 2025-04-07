adfbd6cbbaa2321677ed246c153ef840d348fd5d
Kacper Szuster <kacperszuster16@gmail.com>
2025-04-07 22:01:47 +0200

    Feat: Grid class with functional methods resetGrid and showGrid

--- Program.cs
+++ Program.cs
@@ -2,17 +2,60 @@
 
 namespace ShipGame
 {
+    //public class Ship
+    //{
+
+    //}
     public class Grid
     {
+        private string[,] grid = new string[10,10];
+        public void resetGrid()
+        {
+            for (int i = 0; i < 10;  i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    grid[i, j] = " ";
+                }
+            }
+        }
         public void showGrid()
         {
+            for (int i = 0; i < 10 ; i++)
+            {
+                for (int j = 0; j < 10 ; j++)
+                {
+                    Console.Write(grid[i,j]);
+                    if (j != 9)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+                Console.WriteLine();
 
+                if (i != 9)
+                {
+                    Console.WriteLine("--+---+---+---+---+---+---+---+---+--");
+                }
+            }
         }
+
+        //public void interactiveGrid()
+        //{
+        //    bool endMove = false;
+        //    do
+        //    {
+        //        Console.ReadKey();
+        //    } while (endMove = false);
+        //}
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Grid grid = new Grid();
+            grid.resetGrid();
+            grid.showGrid();
             Console.WriteLine("Hello World!");
         }
     }
